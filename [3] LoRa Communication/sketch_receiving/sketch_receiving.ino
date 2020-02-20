32def291470521a3b2b2b72520f04dd6accb3b9d
Ping Charoenwet <wachiraph.c@gmail.com>
2020-02-20 16:54:51 +0700

    update simple LoRa communication

--- [3] LoRa Communication/sketch_receiving/sketch_receiving.ino
+++ [3] LoRa Communication/sketch_receiving/sketch_receiving.ino
@@ -1,15 +1,42 @@
 /*
  * NSM Cansat Bootcamp
  * LoRa Communication - Receiving
+ * https://circuitdigest.com/microcontroller-projects/arduino-lora-sx1278-interfacing-tutorial
+ * https://iotdesignpro.com/projects/lora-communication-between-two-arduino-using-LoRa-Module-SX1278
+ * https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
  */
+
+#include <LoRa.h>
+#define lora_frequency 433E6;
 
 void setup() {
   
   Serial.begin(115200); 
+
+  while (!Serial); 
+
+  Serial.println("LoRa Communication - Receiving");
+
+  if (!LoRa.begin(lora_frequency)) {
+    Serial.println("LoRa Communication - Start Failed");
+    while (1);
+  }
   
 }
 
 void loop() {
+
+  int packet_size = LoRa.parsePacket();
   
+  if (packet_size) {
+    Serial.print("Received packet '");
+  
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+    
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
 
 }
--- [3] LoRa Communication/sketch_sending/sketch_sending.ino
+++ [3] LoRa Communication/sketch_sending/sketch_sending.ino
@@ -1,15 +1,43 @@
 /*
  * NSM Cansat Bootcamp
  * LoRa Communication - Sending
+ * https://circuitdigest.com/microcontroller-projects/arduino-lora-sx1278-interfacing-tutorial
+ * https://iotdesignpro.com/projects/lora-communication-between-two-arduino-using-LoRa-Module-SX1278
+ * https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
  */
+
+#include <LoRa.h>
+#define lora_frequency 433E6;
+
+unsigned long start_time_counter;
 
 void setup() {
   
-  Serial.begin(115200); 
+  Serial.begin(115200);
+  
+  while (!Serial); 
+
+  Serial.println("LoRa Communication - Sending");
+
+  if (!LoRa.begin(lora_frequency)) {
+    Serial.println("LoRa Communication - Start Failed");
+    while (1);
+  }
   
 }
 
 void loop() {
+
+  start_time_counter = millis();
   
+  LoRa.beginPacket();
+  LoRa.print("Recent Broadcasting Time: ");
+  LoRa.print(start_time_counter);
+  LoRa.endPacket();
+
+  Serial.print("Recent Broadcasting Time: ");
+  Serial.println(start_time_counter);
+
+  delay(5000);
 
 }
