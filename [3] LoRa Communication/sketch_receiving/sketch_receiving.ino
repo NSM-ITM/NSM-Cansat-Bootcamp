0d85caecebd13a915873571e891be15985c44a75
Ping Charoenwet <wachiraph.c@gmail.com>
2020-02-20 11:51:05 +0700

    add another task

---
+++ [3] LoRa Communication/sketch_receiving/sketch_receiving.ino
@@ -0,0 +1,15 @@
+/*
+ * NSM Cansat Bootcamp
+ * LoRa Communication - Receiving
+ */
+
+void setup() {
+  
+  Serial.begin(115200); 
+  
+}
+
+void loop() {
+  
+
+}
---
+++ [3] LoRa Communication/sketch_sending/sketch_sending.ino
@@ -0,0 +1,15 @@
+/*
+ * NSM Cansat Bootcamp
+ * LoRa Communication - Sending
+ */
+
+void setup() {
+  
+  Serial.begin(115200); 
+  
+}
+
+void loop() {
+  
+
+}
